0c1e5ab34e6b8e871197453d0564f92aea1ac3e9
Jud-e <lenuanwa@gmail.com>
2023-01-16 20:22:49 -0800

    update

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 Widget xMargin(double x) {
   return SizedBox(width: x);
@@ -12,5 +13,35 @@ Widget text(String text, double fontSize, Color color) {
   return Text(
     text,
     style: TextStyle(fontSize: fontSize, color: color),
+  );
+}
+
+Widget tracker(IconData icon, String text1, String text2, Color color) {
+  return Container(
+    decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(10)),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            icon,
+            size: 15,
+            color: color,
+          ),
+          xMargin(5),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              text(text1, 10, Colors.grey),
+              text(text2, 13, Colors.black)
+            ],
+          )
+        ],
+      ),
+    ),
   );
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -83,124 +83,141 @@ class _HomeState extends State<Home> {
                   border: Border.all(),
                   borderRadius: BorderRadius.circular(20)),
               child: Padding(
-                padding: const EdgeInsets.fromLTRB(5, 10, 5, 5),
+                padding: const EdgeInsets.fromLTRB(3, 10, 3, 3),
                 child: Column(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  border: Border.all(),
-                                  borderRadius: BorderRadius.circular(50)),
-                              child: const Padding(
-                                padding: EdgeInsets.all(10.0),
-                                child: Icon(Icons.search),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 5, 0, 3),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Icon(Icons.search),
+                                ),
                               ),
-                            ),
-                            xMargin(10),
-                            Column(
-                              children: [
-                                text("Glucose: 5.9 mmol/L", 14, Colors.white),
-                                text("You're on a good slte", 13,
-                                    Color.fromARGB(255, 230, 230, 230))
-                              ],
-                            )
-                          ],
-                        ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.more_horiz,
-                              color: Colors.white,
-                            ))
-                      ],
+                              xMargin(10),
+                              Column(
+                                children: [
+                                  text("Glucose: 5.9 mmol/L", 14, Colors.white),
+                                  text("You're on a good slte", 13,
+                                      const Color.fromARGB(255, 230, 230, 230))
+                                ],
+                              )
+                            ],
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.more_horiz,
+                                color: Colors.white,
+                              ))
+                        ],
+                      ),
                     ),
                     yMargin(7),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Icons.arrow_upward,
-                                  size: 15,
-                                  color: Colors.green,
-                                ),
-                                xMargin(5),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    text("High", 10, Colors.grey),
-                                    text("8.5 mmol/L", 13, Colors.black)
-                                  ],
-                                )
-                              ],
+                        // tracker(Icon(Icons.arrow_upward), "High", "8.5 mmol/L",
+                        //     Colors.green),
+                        Flexible(
+                          flex: 3,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Icon(
+                                    Icons.arrow_upward,
+                                    size: 12,
+                                    color: Colors.green,
+                                  ),
+                                  xMargin(5),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      text("High", 10, Colors.grey),
+                                      text("8.5 mmol/L", 13, Colors.black)
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Icons.compare_arrows,
-                                  size: 15,
-                                  color: Colors.black,
-                                ),
-                                xMargin(5),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    text("Normal", 10, Colors.grey),
-                                    text("5.5 mmol/L", 13, Colors.black)
-                                  ],
-                                )
-                              ],
+                        Flexible(
+                          flex: 3,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Icon(
+                                    Icons.compare_arrows,
+                                    size: 12,
+                                    color: Colors.black,
+                                  ),
+                                  xMargin(5),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      text("Normal", 10, Colors.grey),
+                                      text("5.5 mmol/L", 13, Colors.black)
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Icons.arrow_downward,
-                                  size: 15,
-                                  color: Colors.red,
-                                ),
-                                xMargin(5),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    text("Low", 10, Colors.grey),
-                                    text("3.5 mmol/L", 13, Colors.black)
-                                  ],
-                                )
-                              ],
+                        Flexible(
+                          flex: 3,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Icon(
+                                    Icons.arrow_downward,
+                                    size: 12,
+                                    color: Colors.red,
+                                  ),
+                                  xMargin(5),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      text("Low", 10, Colors.grey),
+                                      text("3.5 mmol/L", 13, Colors.black)
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
@@ -210,6 +227,23 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
+            yMargin(10),
+            Row(
+              children: [
+                Flexible(
+                    flex: 2,
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [text("SYS", 15, Colors.black)],
+                          )
+                        ],
+                      ),
+                    )),
+                Flexible(flex: 2, child: Container())
+              ],
+            )
           ]),
         ),
       ),
