68fcd33c15632a2c4f05bac02c53bdd2e5871650
Jud-e <lenuanwa@gmail.com>
2023-01-24 10:41:20 -0800

    update 24/01

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 var greyBg = const Color(0xFFF4F6FA);
-var darker = Color.fromARGB(255, 131, 131, 131);
+var darker = const Color.fromARGB(255, 131, 131, 131);
 var darkGrey = const Color.fromARGB(255, 199, 199, 200);
 var blueText = const Color(0xFF5E9FF2);
 var blueIcon = const Color(0xFF0B64D9);
